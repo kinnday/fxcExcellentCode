32719f64b23445c2c7bc984b8800ad9faf9a64ab
fu.xc <18911825713@189.cn>
2020-12-23 16:09:50 +0800

    雇工模式是行为模式的一种，它为一组类提供通用的功能，而不需要类实现这些功能，它是命令模式的一种扩展

--- src/main/java/com/design23/group4Extend/e3Emploree/Cleanable.java
+++ src/main/java/com/design23/group4Extend/e3Emploree/Cleanable.java
@@ -1,5 +1,28 @@
 package com.design23.group4Extend.e3Emploree;
 
+//可以被清洁的对象
 public interface Cleanable {
-    void cleaned();
+    //被清洁
+    public void celaned();
+}
+
+//花园
+class Garden implements Cleanable {
+    public void celaned() {
+        System.out.println("花园被清洁干净");
+    }
+}
+
+//厨房
+class Kitchen implements Cleanable {
+    public void celaned() {
+        System.out.println("厨房被清洁干净");
+    }
+}
+
+//衣服
+class Cloth implements Cleanable {
+    public void celaned() {
+        System.out.println("衣服被清洁干净");
+    }
 }
---
+++ src/main/java/com/design23/group4Extend/e3Emploree/Cleaner.java
@@ -0,0 +1,10 @@
+package com.design23.group4Extend.e3Emploree;
+
+//抽象的清洁者
+public class Cleaner {
+
+    //清洁
+    public void clean(Cleanable clean) {
+        clean.celaned();
+    }
+}
